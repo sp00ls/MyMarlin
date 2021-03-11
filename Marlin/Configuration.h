9d34ffac0a6c7c3c4b8b9eff3d425ff3f9e9edd2
Ken <ken@sp00l.org>
2021-03-11 00:15:48 -0500

    Update PIDs for CR10

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  22.20
-    #define DEFAULT_Ki   1.08
-    #define DEFAULT_Kd 114.00
+    #define DEFAULT_Kp  26.16
+    #define DEFAULT_Ki   2.23
+    #define DEFAULT_Kd 76.54
   #endif
 #endif // PIDTEMP
 
@@ -540,9 +540,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 356.34
-  #define DEFAULT_bedKi 70.15
-  #define DEFAULT_bedKd 452.51
+  #define DEFAULT_bedKp 50.10
+  #define DEFAULT_bedKi 9.77
+  #define DEFAULT_bedKd 171.37
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
